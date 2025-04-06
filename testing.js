c866765e9dd60f9bf17976c381a7501271306bb6
Shiv <shiviyer55x@gmail.com>
2025-04-06 19:47:57 +0800

    parsed response data; updated call to use global header

--- testing.js
+++ testing.js
@@ -12,12 +12,19 @@ const API_HEADERS = {
 // functions
 async function testCall() {
     const response = await axios.get("https://api.clashroyale.com/v1/players/%23P2CR80Y2P", {
-        headers: {
-            Authorization: `Bearer ${process.env.API_TOKEN}`
-        }
+        headers: API_HEADERS
     });
 
-    console.log(response.data);
+    const tag = response.data.tag;
+    const name = response.data.name;
+    const clan = response.data.clan.name;
+    const previousFinish = response.data.leagueStatistics.previousSeason.rank;
+    const bestFinish = response.data.leagueStatistics.bestSeason.rank;
+    const favoriteCard = response.data.currentFavouriteCard.name;
+    
+    console.log("Player name: " + name);
+
+    //console.log(response.data);
 }
 
 testCall();
